a0e400602c35d04086dbb3b48f62b33da5de6a8e
Ahmad Hadwan <ahmadhadwan2004@gmail.com>
2024-05-23 06:36:12 +0300

    Removed the rickroll... for now

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function forcehttps() {
     if (location.protocol !== 'https:' &&
         location.protocol !== 'file:') {
-        //location.replace(`https:${location.href.substring(location.protocol.length)}`);
-        location.replace("https://youtu.be/dQw4w9WgXcQ?si=IClE_zPHLFJfK_uf");
+        location.replace(`https:${location.href.substring(location.protocol.length)}`);
+        //location.replace("https://youtu.be/dQw4w9WgXcQ?si=IClE_zPHLFJfK_uf");
     }
 }
 
